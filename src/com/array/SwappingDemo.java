1213381095cf5cbc449c54f16a4047d230cd156a
Tushar <tusharbhosale214@gmail.com>
2024-12-11 17:46:23 +0530

    Added swapping program

---
+++ src/com/array/SwappingDemo.java
@@ -0,0 +1,22 @@
+package com.array;
+
+public class SwappingDemo {
+	public static void main(String[] args) {
+		int a=10,b=20;
+		
+		System.out.println("value of A "+a);
+		System.out.println("value of B "+b);
+		
+		
+		
+		int temp=a;
+		a=b;
+		b=temp;
+		System.out.println("------------------------------------------");
+		
+		System.out.println("value of A "+a);
+		System.out.println("value of B "+b);
+		
+	}
+
+}
